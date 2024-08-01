0ca7c5c3a96f3b4fab5b4e0411a613c709fa895a
endingofourlife <endingofourlife@gmail.com>
2024-08-01 16:03:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const surpriseButton = document.querySelector(".btn-surprise");
+    const surpriseMessage = document.querySelector(".surprise-message");
+
+    surpriseButton.addEventListener("click", function () {
+        surpriseMessage.classList.toggle("hidden");
+        surpriseMessage.classList.add("animate");
+
+        setTimeout(() => {
+            surpriseMessage.classList.remove("animate");
+        }, 3000);
+    });
+});
